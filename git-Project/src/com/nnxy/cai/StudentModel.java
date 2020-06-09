4f5cb987c960091a87bbef558594fd5351936f3b
zhangzhocuai <1312258996qq.com>
2020-06-09 17:27:49 +0800

    第一次提交

---
+++ git-Project/src/com/nnxy/cai/StudentModel.java
@@ -0,0 +1,43 @@
+package com.nnxy.cai;
+
+public class StudentModel {
+
+	private int id;
+	private String username;
+	private String passwor;
+	private int sex;
+	private String love;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPasswor() {
+		return passwor;
+	}
+	public void setPasswor(String passwor) {
+		this.passwor = passwor;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+	
+	
+}
